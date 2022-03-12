8c07517dc242968fd7126a69eb88f07a020a5f2b
Nabil Hadi <napilfadl@gmail.com>
2022-03-12 18:14:43 +0300

    styled home page

--- src/home-page.js
+++ src/home-page.js
@@ -1,10 +1,18 @@
 export default function loadMainPage(containerDiv) {
   const h1 = document.createElement("h1");
   h1.textContent = "Greatest Restaurant of all time";
+  h1.classList.add("mb-2");
   containerDiv.append(h1);
 
-  const shortP = document.createElement("p");
-  shortP.textContent =
-    "Cupcake ipsum dolor sit amet dessert macaroon candy oat cake. Cake sweet gingerbread gummies muffin cake sesame snaps lollipop lemon drops. Pastry caramels cotton candy macaroon tart cotton candy. Danish donut cheesecake cookie shortbread donut powder bonbon chocolate cake.";
-  containerDiv.append(shortP);
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card");
+  containerDiv.append(cardContainer);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = "Why?";
+  cardContainer.append(h2);
+
+  const p = document.createElement("p");
+  p.textContent = "Beacuse I Said So.";
+  cardContainer.append(p);
 }
