f8d532fe040e5c5932d899c71d9bc4d018f0a5c9
Nabil Hadi <napilfadl@gmail.com>
2022-03-12 18:36:40 +0300

    styled Menu Page

--- src/menu-page.js
+++ src/menu-page.js
@@ -1,10 +1,35 @@
+import dishImage from "./dish.jpg";
+
+// Photo by <a href="https://unsplash.com/@widenka?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Martin Widenka</a> on <a href="https://unsplash.com/s/photos/dish?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
 export default function loadMenuPage(containerDiv) {
   const h1 = document.createElement("h1");
   h1.textContent = "Restaurant Menu!!";
+  h1.classList.add("mb-2");
   containerDiv.append(h1);
 
-  const shortP = document.createElement("p");
-  shortP.textContent =
-    "Cupcake ipsum dolor sit amet dessert macaroon candy oat cake. Cake sweet gingerbread gummies muffin cake sesame snaps lollipop lemon drops. Pastry caramels cotton candy macaroon tart cotton candy. Danish donut cheesecake cookie shortbread donut powder bonbon chocolate cake.";
-  containerDiv.append(shortP);
+  const gridContainer = document.createElement("div");
+  gridContainer.classList.add("grid-container");
+  containerDiv.append(gridContainer);
+
+  for (let i = 0; i < 6; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item", "card");
+    gridContainer.append(gridItem);
+
+    const gridItemImage = document.createElement("img");
+    gridItemImage.classList.add("grid-item-img");
+    gridItemImage.setAttribute("src", dishImage);
+    gridItem.append(gridItemImage);
+
+    const gridItemTitle = document.createElement("h3");
+    gridItemTitle.classList.add("grid-item-title");
+    gridItemTitle.textContent = `Dish #${i + 1}`;
+    gridItem.append(gridItemTitle);
+
+    const gridItemDesc = document.createElement("p");
+    gridItemDesc.classList.add("grid-item-desc");
+    gridItemDesc.textContent = `Our finest Meal`;
+    gridItem.append(gridItemDesc);
+  }
 }
