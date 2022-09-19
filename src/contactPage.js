ad6f994b5e38efa18378e9c39aec1d42b62ca664
Nabil Hadi <napilfadl@gmail.com>
2022-09-19 12:11:39 +0200

    Add content for Contact page

--- src/contactPage.js
+++ src/contactPage.js
@@ -1,5 +1,35 @@
+import { createElement } from "./utils";
+
 function loadContactPage(container) {
-  container.textContent = "Contact Page";
+  const contactPageContainer = createElement({
+    classNames: ["contact-page-container"],
+  });
+  contactPageContainer.appendChild(
+    createElement({
+      tag: "h1",
+      textContent: "Contact Us If you liked the food!",
+    })
+  );
+
+  contactPageContainer.appendChild(
+    createElement({
+      textContent: "555 555 555",
+    })
+  );
+
+  contactPageContainer.appendChild(
+    createElement({
+      textContent: "Some place in earth",
+    })
+  );
+
+  contactPageContainer.appendChild(
+    createElement({
+      textContent: "restaurant@greatest-restaurant.com",
+    })
+  );
+
+  container.appendChild(contactPageContainer);
 }
 
 export { loadContactPage };
