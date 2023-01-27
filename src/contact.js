7f8f2ce5624a5a9a4b4a946d71de6e1ba5810d1f
Nabil Hadi <napilfadl@gmail.com>
2023-01-27 11:30:11 +0200

    Add image

--- src/contact.js
+++ src/contact.js
@@ -29,6 +29,16 @@ function displayContactPage(pageContent) {
     })
   );
 
+  contactPageContainer.appendChild(
+    createElement({
+      tag: "img",
+      classNames: ["contact-img"],
+      attributes: {
+        src: "https://images.unsplash.com/photo-1596524430615-b46475ddff6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      },
+    })
+  );
+
   pageContent.appendChild(contactPageContainer);
 }
 
